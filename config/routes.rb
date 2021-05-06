9b019b8a2e863a4b31421d315474154d2e9f3a98
toshi32 <viva.la.vida0522@gmail.com>
2021-05-06 23:24:49 +0900

    update login session

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'users# new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :picture_blogs do
+    collection do
+    post :confirm
+    end
+  end
 end
