52fea4690cdcbb0ce6601dce78d7f5890cef5999
toshi32 <viva.la.vida0522@gmail.com>
2021-05-07 18:16:43 +0900

    test

--- app/helpers/picture_blogs_helper.rb
+++ app/helpers/picture_blogs_helper.rb
@@ -1,2 +1,9 @@
 module PictureBlogsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_blogs_path
+    elsif action_name == 'edit'
+      picture_blog_path
+    end
+  end
 end
