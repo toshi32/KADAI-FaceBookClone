8ee94069510143c45319922fd853b40421a84376
toshi32 <viva.la.vida0522@gmail.com>
2021-05-07 17:43:31 +0900

    edit picture_blog model

--- app/models/picture_blog.rb
+++ app/models/picture_blog.rb
@@ -1,3 +1,10 @@
 class PictureBlog < ApplicationRecord
   belongs_to :user
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_picture_blogs_path
+    elsif action_name == 'edit'
+      blog_path
+    end
+  end
 end
